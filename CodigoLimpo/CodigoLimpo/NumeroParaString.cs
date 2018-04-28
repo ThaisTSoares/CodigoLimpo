c2648e585fbddb2e4b96724d30c38b7f6cde78fd
Thais Soares <thais.soares@groupsoftware.com.br>
2018-04-28 10:48:24 -0300

    unidades

---
+++ CodigoLimpo/CodigoLimpo/NumeroParaString.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodigoLimpo
+{
+    class NumeroParaString
+    {
+        private static string[] UNIDADES = { "zero", "um", "dois", "três", "quatro", "cinco", "seis", "sete", "oito", "nove" };
+
+        public static string unidadeParaTexto(int num)
+        {
+            if(num < UNIDADES.Length)
+                return UNIDADES[num];
+            return "";
+        }
+    }
+}
--- CodigoLimpo/CodigoLimpo/Program.cs
+++ CodigoLimpo/CodigoLimpo/Program.cs
@@ -10,6 +10,8 @@ namespace CodigoLimpo
     {
         static void Main(string[] args)
         {
+           System.Console.WriteLine(NumeroParaString.unidadeParaTexto(1));
+           System.Console.ReadKey();
         }
     }
 }
